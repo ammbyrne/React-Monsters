a2e51a480f5efc702ccac01e0765fa57bd41ced3
ammbyrne <andy_m_byrne@yahoo.co.uk>
2020-09-16 15:25:40 +0800

    Updates

---
+++ precache-manifest.71c81b58140b4806ec236f1586b878a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "159edb5741a69a408aa06521fcab4c3d",
+    "url": "/React_Monsters/React_Monsters/index.html"
+  },
+  {
+    "revision": "db2b25a8101e6d39b609",
+    "url": "/React_Monsters/React_Monsters/static/css/main.cdd1125e.chunk.css"
+  },
+  {
+    "revision": "f76a773505c15eeebcd4",
+    "url": "/React_Monsters/React_Monsters/static/js/2.ff4b387a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React_Monsters/React_Monsters/static/js/2.ff4b387a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db2b25a8101e6d39b609",
+    "url": "/React_Monsters/React_Monsters/static/js/main.7ef88ea7.chunk.js"
+  },
+  {
+    "revision": "3187582ddddc7e31775e",
+    "url": "/React_Monsters/React_Monsters/static/js/runtime-main.13e23460.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React_Monsters/React_Monsters/static/media/logo.5d5d9eef.svg"
+  }
+]);
